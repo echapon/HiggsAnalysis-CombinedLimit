4484813eb6e16bfc1a5d5f80dfaca715bbe04f9a
Emilien Chapon <emilien.chapon@cern.ch>
2018-05-04 20:27:48 +0200

    Update axion stuff

---
+++ test/FSQ16012/limits.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "##################"
+echo "#  mass = 6 GeV  #"
+echo "##################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion0.txt
+
+echo "##################"
+echo "#  mass = 9 GeV  #"
+echo "##################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion1.txt
+
+echo "###################"
+echo "#  mass = 11 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion2.txt
+
+echo "###################"
+echo "#  mass = 14 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion3.txt
+
+echo "###################"
+echo "#  mass = 16 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion4.txt
+
+echo "###################"
+echo "#  mass = 30 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_counting_axion_5.txt
+
+echo "###################"
+echo "#  mass = 90 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_counting_axion_6.txt
--- test/FSQ16012/prepareInputs_axion.C
+++ test/FSQ16012/prepareInputs_axion.C
@@ -7,14 +7,33 @@ using namespace std;
 
 void prepareInputs_axion(int imass) {
    TFile *f = TFile::Open("input_hinvmass.root","UPDATE");
-   TH1F *h = new TH1F(Form("axion%d",imass),"",8,0,20);
+   TH1F *h = new TH1F(Form("axion%d",imass),"",6,5,20);
 
    // arrays
-   double mass[6] = {6, 11, 14, 16, 30, 90};
-   double eff[6] = {0.146523, 0.306750, 0.334480, 0.343131, 0.353650, 0.353818};
-   double acc[6] = {0.763764, 0.873874, 0.895896, 0.916917, 0.967968, 0.996997};
-   double resm[6] = {0.9893, 0.9575, 0.9372, 0.9286, 0.9442, 0.9269};
-   double ress[6] = {0.06092, 0.07542, 0.0891, 0.0846, 0.08925, 0.09091};
+   double mass[7] = {6, 9, 11, 14, 16, 30, 90};
+   double acc[7] = {0.763764, 0.895896, 0.873874, 0.895896, 0.916917, 0.967968, 0.996997};
+
+   // functions for resolution
+   TF1 *fm = new TF1("fm","[0]+[1]*exp([2]*x)",5,20);
+   fm->SetParameters(9.24958e-01,6.17813e-01,-3.47891e-01);
+   TF1 *fr = new TF1("fr","[0]+[1]*exp([2]*x)",5,20);
+   fr->SetParameters(0.0909661,-0.207283,-0.307946);
+
+
+   // function for efficiency
+   TF1 fe("f","[0]*(1-exp((x-[1])/[2]))",0,30);
+   fe.SetParameters(3.32406e-01,4.28904e+00,-2.64850e+00);
+   // binned efficiencies for syst
+   double x[8],y[8];
+   x[0]=5.5; y[0]=0.105285;
+   x[1]=6.5; y[1]=0.203834;
+   x[2]=7.5; y[2]=0.241597;
+   x[3]=9; y[3]=0.271701;
+   x[4]=11; y[4]=0.288428;
+   x[5]=13.5; y[5]=0.317935;
+   x[6]=17.5; y[6]=0.366667;
+   x[7]=25; y[7]=0.309735;
+   TGraph g(8,x,y);
 
    double xsec = 10e-3; // mub
    double lumi = 370.884;  // mub-1
@@ -22,22 +41,22 @@ void prepareInputs_axion(int imass) {
    double sf = 1.12/1.09;
 
    TF1 *fgaus = new TF1("fgaus","gaus",0,200);
-   fgaus->SetParameters(1,mass[imass]*resm[imass],mass[imass]*ress[imass]);
+   fgaus->SetParameters(1, mass[imass]*fm->Eval(mass[imass]), mass[imass]*fr->Eval(mass[imass]));
    // h->FillRandom("fgaus",nevts);
    for (int i=0; i<nevts; i++) {
       double m = fgaus->GetRandom();
       if (m<5) m=5;
       h->Fill(m);
    }
-   h->Scale(xsec*lumi*acc[imass]*eff[imass]/nevts);
-   for (int i=0; i<3; i++) {h->SetBinContent(i,0); h->SetBinError(i,0);}
+   h->Scale(xsec*lumi*acc[imass]*fe.Eval(mass[imass])/nevts);
 
-   cout << mass[imass] << ": " << h->Integral(1,8) << endl;;
+   cout << mass[imass] << ": " << h->Integral(1,6) << endl;;
+   cout << "syst eff: " << (fe.Eval(mass[imass])-g.Eval(mass[imass]))/fe.Eval(mass[imass]) << endl;
 
    f->Write();
    f->Close();
 }
 
 void prepareInputs_axion() {
-   for (int i=0; i<6; i++) prepareInputs_axion(i);
+   for (int i=0; i<7; i++) prepareInputs_axion(i);
 }
